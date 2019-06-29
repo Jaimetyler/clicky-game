b3fd967661308f0b3071fbb9a7edd8db15b06517
Jaime Tyler <jaimetyler88@gmail.com>
2019-06-29 08:25:21 -0400

    swapped compnets to es6 functions

--- src/componets/header.js
+++ src/componets/header.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-const Header = () => (
-  <div className = "header">
-    <h2>Click on an image to earn points, but don't click on any more than once!</h2>
-  </div>
-);
+const Header = () => {
+    return (
+        <div className="header">
+            <h2>Click on an image to earn points, but don't click on any more than once!</h2>
+        </div>
+    )
+};
 
 export default Header;
