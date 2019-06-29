bc8272de51d81fd0243c4ab4736fd3ee7a531640
Jaime Tyler <jaimetyler88@gmail.com>
2019-06-29 08:20:24 -0400

    added basic header componet

---
+++ src/componets/header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => (
+  <div className = "header">
+    <h2>Click on an image to earn points, but don't click on any more than once!</h2>
+  </div>
+);
+
+export default Header;
